7b5931110b76b6780669071916244c2c36e97733
AndreyTHT <andreythtand@gmail.com>
2024-11-17 01:47:58 +0400

    Добавьте файлы проекта.

---
+++ GSC_LR4/Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace GSC_Lr4
+{
+    public partial class Form1 : Form
+    {
+        Bitmap myBitmap;
+        Graphics g;
+        Pen DrawPen = new Pen(Color.Black, 1);
+        List<Point> VertexList = new List<Point>();
+        List<Pgn> pgns = new List<Pgn>();
+        int Operation = 1; // Рисование
+        int checkPgn = -1; // Индекс выбранной фигуры (-1 означает, что ничего не выбрано)
+        Point pictureBox1MousePos = new Point(); // Позиция последнего нажатия
+
+        public Form1()
+        {
+            InitializeComponent();
+            myBitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            g = Graphics.FromImage(myBitmap);
+        }
+
+        // Метод для заполнения списка вершин новой фигуры
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            switch (comboBox2.SelectedIndex)
+            {
+                case 0:
+                    Operation = 2; // Перемещение
+                    break;
+                case 1:
+                    Operation = 3; // Вращение
+                    break;
+                case 2:
+                    Operation = 4; // Масштабирование
+                    break;
+            }
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            pictureBox1MousePos = e.Location;
+
+            switch (Operation)
+            {
+                case 1: // Ввод новой фигуры
+                    InputPgn(e);
+                    break;
+
+                default: // Выбор фигуры
+                    checkPgn = -1;
+                    for (int i = 0; i < pgns.Count; i++)
+                    {
+                        if (pgns[i].ThisPgn(e.X, e.Y))
+                        {
+                            checkPgn = i;
+                            break;
+                        }
+                    }
+                    break;
+            }
+
+            pictureBox1.Image = myBitmap;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (comboBox1.SelectedIndex)
+            {
+                case 0: DrawPen.Color = Color.Black; break;
+                case 1: DrawPen.Color = Color.Red; break;
+                case 2: DrawPen.Color = Color.Green; break;
+                case 3: DrawPen.Color = Color.Blue; break;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            pgns.Clear();
+            VertexList.Clear();
+            Operation = 1;
+            RedrawAll();
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button != MouseButtons.Left || checkPgn < 0 || checkPgn >= pgns.Count)
+                return; // Проверка на корректный индекс
+
+            var selectedPgn = pgns[checkPgn];
+
+            switch (Operation)
+            {
+                case 2: // Перемещение
+                    selectedPgn.Move(e.X - pictureBox1MousePos.X, e.Y - pictureBox1MousePos.Y);
+                    break;
+
+                case 3: // Вращение
+                    selectedPgn.Move(-selectedPgn.GetCenter().X, -selectedPgn.GetCenter().Y);
+                    selectedPgn.Rotate(CalculateAngle(e, selectedPgn));
+                    selectedPgn.Move(selectedPgn.GetCenter().X, selectedPgn.GetCenter().Y);
+                    break;
+
+                case 4: // Масштабирование
+                    selectedPgn.Move(-selectedPgn.GetCenter().X, -selectedPgn.GetCenter().Y);
+                    double scale = CalculateScale(e, selectedPgn);
+                    if (scale > 0) // Убедимся, что масштабирование корректно
+                        selectedPgn.Scale(scale);
+                    selectedPgn.Move(selectedPgn.GetCenter().X, selectedPgn.GetCenter().Y);
+                    break;
+            }
+
+            pictureBox1MousePos = e.Location;
+            RedrawAll();
+        }
+
+        private double CalculateScale(MouseEventArgs e, Pgn selectedPgn)
+        {
+            double lengthNew = Math.Sqrt(Math.Pow(e.X - selectedPgn.GetCenter().X, 2) + Math.Pow(e.Y - selectedPgn.GetCenter().Y, 2));
+            double lengthOld = Math.Sqrt(Math.Pow(pictureBox1MousePos.X - selectedPgn.GetCenter().X, 2) + Math.Pow(pictureBox1MousePos.Y - selectedPgn.GetCenter().Y, 2));
+
+            // Проверка на деление на ноль
+            if (lengthOld == 0) return 1; // Возвращаем масштаб 1 (без изменений)
+
+            return lengthNew / lengthOld;
+        }
+
+        private double CalculateAngle(MouseEventArgs e, Pgn selectedPgn)
+        {
+            PointF A = new PointF(e.X - selectedPgn.GetCenter().X, e.Y - selectedPgn.GetCenter().Y);
+            PointF B = new PointF(pictureBox1MousePos.X - selectedPgn.GetCenter().X, pictureBox1MousePos.Y - selectedPgn.GetCenter().Y);
+            return -Math.Atan2(A.Y, A.X) + Math.Atan2(B.Y, B.X);
+        }
+
+        private void pictureBox1_SizeChanged(object sender, EventArgs e)
+        {
+            // Сохраняем существующий контент при изменении размера
+            Bitmap newBitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            Graphics tempG = Graphics.FromImage(newBitmap);
+            tempG.DrawImage(myBitmap, 0, 0);
+            myBitmap = newBitmap;
+            g = Graphics.FromImage(myBitmap);
+            RedrawAll();
+        }
+
+        private void InputPgn(MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                Point NewP = new Point(e.X, e.Y);
+                VertexList.Add(NewP);
+                int k = VertexList.Count;
+
+                if (k > 1)
+                    g.DrawLine(DrawPen, VertexList[k - 2], VertexList[k - 1]);
+                else
+                    g.DrawRectangle(DrawPen, e.X, e.Y, 1, 1);
+            }
+            else if (e.Button == MouseButtons.Right && VertexList.Count > 2) // Завершение ввода фигуры
+            {
+                Point NewP = new Point(e.X, e.Y);
+                VertexList.Add(NewP);
+                int k = VertexList.Count;
+                if (k > 1)
+                    g.DrawLine(DrawPen, VertexList[k - 1], VertexList[0]);
+                else
+                    g.DrawRectangle(DrawPen, e.X, e.Y, 1, 1);
+                Pgn NewPgn = new Pgn();
+                foreach (var item in VertexList)
+                    NewPgn.Add(item);
+
+                NewPgn.search_center();
+                g.DrawRectangle(new Pen(Color.Red), NewPgn.GetCenter().X, NewPgn.GetCenter().Y, 2, 2);
+                VertexList.Clear();
+
+                pgns.Add(NewPgn);
+                RedrawAll();
+            }
+        }
+
+
+        private void RedrawAll()
+        {
+            g.Clear(pictureBox1.BackColor);
+            foreach (var pgn in pgns)
+            {
+                pgn.Fill(g, DrawPen);
+                pgn.search_center();
+                g.DrawRectangle(new Pen(Color.Red), pgn.GetCenter().X, pgn.GetCenter().Y, 2, 2);
+            }
+            pictureBox1.Image = myBitmap;
+        }
+    }
+}
---
+++ GSC_LR4/Pgn.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+
+
+namespace GSC_Lr4
+{
+   public class Pgn
+    {
+        List<PointF> VertexList;
+        int Ymax = 0, Ymin = 0;
+        PointF center;
+        int only_one = 0;
+        public Pgn() { VertexList = new List<PointF>(); }
+
+        // метод Добавление вершины
+        public void Add(Point NewVertex) { VertexList.Add(NewVertex); }
+
+        public void search_center() // поиск центра нашей фигуры
+        {
+            float x = 0, y = 0;
+            for (int i = 0; i < VertexList.Count; i++)
+            {
+                x += VertexList[i].X;
+                y += VertexList[i].Y;
+            }
+            center.X = x / VertexList.Count;
+            center.Y = y / VertexList.Count;
+            
+        }
+        public PointF GetCenter() { return center; }
+
+        private void SearchY() // Высчитывает наивысшую и наименьшую точку нашей фигуры
+        { 
+            for (int i = 0; i < VertexList.Count(); i++)
+            {
+                if (Ymax < VertexList[i].Y)
+                    Ymax = (int)VertexList[i].Y;
+                if (Ymin > VertexList[i].Y)
+                    Ymin = (int)VertexList[i].Y;
+            }
+        }
+        public void Fill(Graphics g, Pen DrawPen)
+        {
+            if(only_one == 0)   SearchY(); only_one++;
+            int k;
+            for (int value = (int)Ymin; value < Ymax; value++) // проход по фигуре в координате Y
+            {
+                List<int> point = new List<int>();
+                for (int i = 0; i < VertexList.Count; i++)
+                {
+                    if (i < VertexList.Count - 1) k = i + 1; else k = 0;
+                    if ((VertexList[i].Y < value && VertexList[k].Y >= value) || (VertexList[i].Y >= value && VertexList[k].Y < value))
+                    {
+                        int x1;
+                        double x = ((VertexList[k].X - VertexList[i].X) * (value - VertexList[i].Y) / (VertexList[k].Y - VertexList[i].Y)) + VertexList[i].X; // высчитывание точки пересечения
+                        
+                        if ((point.Count % 2 != 0) || point.Count != 0) x1 = (int)Math.Ceiling(x); else x1 = (int)Math.Floor(x);
+
+                        point.Add(x1);
+                    }
+                }
+                point.Sort();
+                for (int i = 0; i < point.Count(); i += 2) // зарисовываем фигуру по найденым точкам
+                    g.DrawLine(DrawPen, new Point(point[i], value), new Point(point[i + 1], value));
+            }
+            
+        }
+
+        // выделение многоугольника
+        public bool ThisPgn(int mX, int mY)
+        {
+            int n = VertexList.Count() - 1, k = 0, m = 0;
+            PointF Pi, Pk; 
+            bool check = false;
+            for (int i = 0; i <= n; i++)
+            {
+                if (i < n) k = i + 1; else k = 0;
+                Pi = VertexList[i]; Pk = VertexList[k];
+                if ((Pi.Y < mY) & (Pk.Y >= mY) | (Pi.Y >= mY) & (Pk.Y < mY))
+                    if ((mY - Pi.Y) * (Pk.X - Pi.X) / (Pk.Y - Pi.Y) + Pi.X < mX) m++;
+            }
+            if (m % 2 == 1) check = true;
+            return check;
+        }
+
+        private double[,] Multiplication(double[,] a, double[,] b) // умножение матриц
+        {
+            double[,] r = new double[a.GetLength(0), b.GetLength(1)];
+            for (int i = 0; i < a.GetLength(0); i++)
+            {
+                for (int j = 0; j < b.GetLength(1); j++)
+                {
+                    for (int k = 0; k < b.GetLength(0); k++)
+                    {
+                        r[i, j] += a[i, k] * b[k, j];
+                    }
+                }
+            }
+            return r;
+        }
+        // плоско-параллельное перемещение
+        public void Move(double dx, double dy)
+        {
+            int n = VertexList.Count() - 1;
+            PointF fP = new PointF();
+            double[,] A = new double[1,3];
+            double[,] B = new double[3,3];
+            for(int i = 0; i < B.GetLength(0);i++)
+            {
+                for(int j = 0;j < B.GetLength(1);j++)
+                {
+                    if (i - j == 0) B[i, j] = 1;
+                    if (i == 2)
+                        if (j == 0) B[i, j] = dx; else if (j == 1) B[i, j] = dy;
+                }
+            }
+            for (int i = 0; i <= n; i++)
+            {
+                A[0, 0] = VertexList[i].X; A[0, 1] = VertexList[i].Y; A[0, 2] = 1;
+                double[,] result = Multiplication(A, B);
+                fP.X = (float)result[0,0]; fP.Y = (float)result[0, 1];
+                VertexList[i] = fP;
+            }
+            SearchY();
+        }
+
+        public void Rotate(double angle) // поворот фигуры
+        {
+            int n = VertexList.Count() - 1;
+            PointF fP = new PointF();
+            double[,] A = new double[1, 3];
+            double[,] B = new double[3, 3];
+            B[2, 2] = 1;
+            B[0, 0] = B[1,1] = Math.Cos(angle);
+            B[0, 1] = -Math.Sin(angle);
+            B[1, 0] = Math.Sin(angle);
+
+            for (int i = 0; i <= n; i++)
+            {
+                A[0, 0] = VertexList[i].X; A[0, 1] = VertexList[i].Y; A[0, 2] = 1;
+                double[,] result = Multiplication(A, B);
+                fP.X = (float)result[0, 0]; fP.Y = (float)result[0, 1];
+                VertexList[i] = fP;
+            }
+            SearchY();
+        }
+
+        public void Scale(double scale)// изменение размеров фигуры
+        {
+            int n = VertexList.Count() - 1;
+            PointF fP = new PointF();
+            double[,] A = new double[1, 3];
+            double[,] B = new double[3, 3];
+            B[0, 0] = B[1,1] = scale; B[2, 2] = 1;
+            for (int i = 0; i <= n; i++)
+            {
+                A[0, 0] = VertexList[i].X; A[0, 1] = VertexList[i].Y; A[0, 2] = 1;
+                double[,] result = Multiplication(A, B);
+                fP.X = (float)result[0, 0]; fP.Y = (float)result[0, 1];
+                VertexList[i] = fP;
+            }
+            SearchY();
+        }
+        public void Clear()
+        {
+            VertexList.Clear();
+        }
+
+    }
+}
